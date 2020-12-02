60cfd33bfcd16e7fec9afd2408c0679a958a868e
ZeroBone <zerobone21@gmail.com>
2020-12-02 22:16:19 +0100

    Added new test cases for complicated proofs

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ def test_case_2():
 
 def test_case_3():
     # This test case should return True
+    # in this test case we prove a -> a
 
     a = Variable("a")
 
@@ -297,6 +298,146 @@ def test_case_10():
     return test.verify()
 
 
+# constructs a proof of a -> c assuming a -> b and b -> c are proven
+def prove_implication_transitivity(a, b, c):
+
+    ab = Implies(a, b)
+    bc = Implies(b, c)
+    ac = Implies(a, c)
+
+    ax2_q = Implies(a, bc)
+
+    ax2_r = Implies(ab, ac)
+
+    return [
+        # Axiom 1
+        Implies(
+            Implies(
+                Implies(a, bc),
+                Implies(ab, ac)
+            ),
+            Implies(
+                bc,
+                Implies(
+                    Implies(a, bc),
+                    Implies(ab, ac)
+                )
+            )
+        ),
+        # Axiom 2
+        Implies(
+            Implies(a, bc),
+            Implies(ab, ac)
+        ),
+        # Modus ponens
+        Implies(
+            bc,
+            Implies(
+                Implies(a, bc),
+                Implies(ab, ac)
+            )
+        ),
+        # Axiom 2
+        Implies(
+            Implies(bc, Implies(ax2_q, ax2_r)),
+            Implies(Implies(bc, ax2_q), Implies(bc, ax2_r))
+        ),
+        # Modus ponens
+        Implies(Implies(bc, ax2_q), Implies(bc, ax2_r)),
+        # Axiom 1
+        Implies(bc, Implies(a, bc)),
+        # Modus ponens
+        Implies(bc, Implies(ab, ac)),
+        # Unfold this formula with modus ponens to get a -> c
+        Implies(ab, ac),
+        ac
+    ]
+
+
+def test_case_11():
+    # This test case should return True
+
+    a = Variable("a")
+    b = Variable("a")
+    c = Variable("a")
+
+    assumptions = [
+        Implies(a, b),
+        Implies(a, c)
+    ]
+
+    proof = prove_implication_transitivity(a, b, c)
+
+    test = Proof(assumptions, proof)
+
+    return test.verify()
+
+
+def test_case_12():
+    # This test case should return True
+
+    a = Variable("a")
+
+    aa = Implies(a, a)
+    a_aa = Implies(a, aa)
+
+    n_a_aa = Not(a_aa)
+    nn_a_aa = Not(n_a_aa)
+
+    na = Not(a)
+    nna = Not(na)
+
+    assumptions = [
+        Implies(
+            a_aa,
+            Implies(Implies(a_aa, a), a)
+        )
+    ]
+
+    proof = [
+        # Axiom 1
+        a_aa,
+        # Axiom 3
+        Implies(Implies(nn_a_aa, nna), Implies(na, n_a_aa)),
+        # Axiom 3
+        Implies(Implies(na, n_a_aa), Implies(a_aa, a)),
+        # Combine last 2 steps
+        *prove_implication_transitivity(
+            Implies(nn_a_aa, nna),
+            Implies(na, n_a_aa),
+            Implies(a_aa, a)
+        ),
+        # It is now proven that:
+        Implies(Implies(nn_a_aa, nna), Implies(a_aa, a)),
+        # Axiom 1
+        Implies(nna, Implies(nn_a_aa, nna)),
+        # Combine last 2 steps
+        *prove_implication_transitivity(
+            nna,
+            Implies(nn_a_aa, nna),
+            Implies(a_aa, a)
+        ),
+        # It is now proven that
+        Implies(nna, Implies(a_aa, a)),
+        # Modus ponens for the assumption and the first formula
+        Implies(Implies(a_aa, a), a),
+        # Combine the last step with proof[6]
+        *prove_implication_transitivity(
+            nna,
+            Implies(a_aa, a),
+            a
+        ),
+        # We have proven that
+        Implies(nna, a)
+    ]
+
+    print(len(proof))
+
+    test = Proof(assumptions, proof)
+
+    return test.verify()
+
+
 if __name__ == "__main__":
     print("Test case 1:")
     t1 = test_case_1()
@@ -347,6 +488,16 @@ if __name__ == "__main__":
     t10 = test_case_10()
     print("Proof correct: ", t10)
     assert t10, "Test 10 failed"
+
+    print("Test case 11:")
+    t11 = test_case_11()
+    print("Proof correct: ", t11)
+    assert t11, "Test 11 failed"
+
+    print("Test case 12:")
+    t12 = test_case_12()
+    print("Proof correct: ", t12)
+    assert t12, "Test 12 failed"
 
     print("---")
     print("All tests passed!")
